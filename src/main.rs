900b2c7a66270b29564065df65080f846dcfefc7
raulacor <raul.acorsi@gmail.com>
2026-02-18 12:55:33 -0300

    Building the deck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::seq::SliceRandom;
+
 #[derive(Debug, Clone, Copy)]
 enum Suit {
     Hearts,
@@ -21,5 +23,45 @@ enum Rank {
     Queen,
     King,
     Ace,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Card {
+    suit: Suit,
+    rank: Rank,
+}
+
+struct Deck {
+    cards: Vec<Card>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = [Suit::Hearts, Suit::Diamonds, Suit::Clubs, Suit::Spades];
+        let ranks = [
+            Rank::Two,
+            Rank::Three,
+            Rank::Four,
+            Rank::Five,
+            Rank::Six,
+            Rank::Seven,
+            Rank::Eight,
+            Rank::Nine,
+            Rank::Ten,
+            Rank::Jack,
+            Rank::Queen,
+            Rank::King,
+            Rank::Ace,
+        ];
+        let mut cards = Vec::new();
+
+        for suit in suits {
+            for rank in ranks {
+                cards.push(Card { suit, rank });
+            }
+        }
+        Deck { cards }
+    }
+    fn shuffle(&mut self) {}
 }
 fn main() {}
